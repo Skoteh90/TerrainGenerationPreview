c7f6c7e1105526086045a24ffd53c8e7317871d2
Scott Grambo <Scottgrambo@live.com>
2019-10-01 17:14:20 -0700

    Readme update and threading preview source code added.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # TerrainGenerationPreview
 
+This is a preview resource. The bulk of the code is private and will be utilized in a future game.
+You can preview the queue, async coroutine, and multithreading system within ThreadedDataRequester.cs file.
+
+### World Preview
 ![World Preview](preview.png)
 
+### Generation Preview
 ![Generation Preview](generation_preview.gif)
 
+### Level Of Detail Preview
 ![Level Of Detail Preview](level_of_detail_preview.gif)
---
+++ ThreadedDataRequester.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class ThreadedDataRequester : MonoBehaviour {
+
+	static ThreadedDataRequester instance;
+	Queue<ThreadInfo> dataQueue = new Queue<ThreadInfo>();
+
+	bool disabled;
+	
+	float internalLogicUpdateSpeed = 0.04f; // Dequeue Speed for threaded data requests.
+
+	private void Start()
+	{
+		StartCoroutine("LogicUpdate");
+	}
+
+	IEnumerator LogicUpdate() {
+		while (!disabled)
+		{
+			yield return new WaitForSeconds(internalLogicUpdateSpeed);
+			if (dataQueue.Count > 0) {
+				ThreadInfo threadInfo = dataQueue.Dequeue ();
+				threadInfo.callback (threadInfo.parameter);
+			}
+		}
+	}
+	
+	public void SetInternalLogicUpdateSpeed(float value)
+	{
+		internalLogicUpdateSpeed = value;
+	}
+	
+	void Awake() {
+		instance = FindObjectOfType<ThreadedDataRequester>();
+	}
+
+	public static void RequestData(Func<object> generateData, Action<object> callback) {
+		ThreadStart threadStart = delegate {
+			instance.DataThread (generateData, callback);
+		};
+
+		new Thread (threadStart).Start ();
+	}
+
+	void DataThread(Func<object> generateData, Action<object> callback) {
+		object data = generateData ();
+		lock (dataQueue) {
+			dataQueue.Enqueue (new ThreadInfo (callback, data));
+		}
+	}
+
+	struct ThreadInfo {
+		public readonly Action<object> callback;
+		public readonly object parameter;
+
+		public ThreadInfo (Action<object> callback, object parameter)
+		{
+			this.callback = callback;
+			this.parameter = parameter;
+		}
+
+	}
+}
